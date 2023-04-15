61eefe577d8172d5e4f1709321c136fe8499f469
shilpi <shilpi8624@gmail.com>
2023-04-15 15:42:27 +0530

    :tada: home screen done

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  StyleSheet,  View } from 'react-native';
+
+import ProductsScreen from './src/screens/productsScreen';
 
 export default function App() {
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ProductsScreen/>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +25,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+      width:"100%",
+      aspectRatio:1,
+  },
+  itemContainer:{
+    width: "50%",
+    padding:1,
+  }
 });
